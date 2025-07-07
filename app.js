ac27e10ea0d2dff751cbda2ceae878aa35549820
rudratejareddy07 <rudratejareddy07@gmail.com>
2025-07-07 15:19:29 +0530

    error handling

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app=express();
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
+const wrapAsync=require("./util/wrapAsync.js");
+const ExpressError=require("./util/ExpressError.js");
+const {listingSchema}=require("./schema.js");
 
 app.engine('ejs',ejsMate);
 
@@ -50,54 +53,76 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+const listingValidate=(req,res,next)=>{
+    const {error}=listingSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(402,result.error);
+    }else{
+        next();
+    }
+}
 app.get("/",(req,res)=>{
     res.send("I amm root");
 })
-app.get("/listings",async(req,res)=>{
-    const allListings=await Listing.find({})
-    
-   
+app.get("/listings",wrapAsync(async(req,res)=>{
+    const allListings=await Listing.find({});
+    allListings.forEach((listing) => {
+    listing.priceFormatted = listing.price
+      ? listing.price.toLocaleString("en-IN")
+      : "N/A";
+  });
     res.render("listings/index",{allListings});
     
-})
+}))
 
 
 //create route
-app.get("/listings/new",(req,res)=>{
+app.get("/listings/new",wrapAsync(async(req,res)=>{
     res.render("listings/new");
-})
+}))
 
 //edit route
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     const {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/edit",{listing});
-})
+}))
 
 //show route
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     const {id}=req.params;
     const listing=await Listing.findById(id);
-    listing.priceFormatted = listing.price.toLocaleString('en-IN');
+     if (listing.price != null) {
+    listing.price = listing.price.toLocaleString('en-IN');
+  } else {
+    listing.price = 'N/A';
+  }
+    
+    
     res.render("listings/show",{listing});
-})
+}))
 
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",wrapAsync(async(req,res)=>{
     const {id}=req.params;
     const newListing=await Listing.findByIdAndUpdate(id,req.body.listing);
     res.redirect("/listings");
 
-})
-app.delete("/listings/:id",async(req,res)=>{
+}))
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     const {id}=req.params;
     const newListing=await Listing.findByIdAndDelete(id);
     req.flash("success", "Listing deleted successfully!");
     res.redirect("/listings");
     
 
-})
+}))
 
-app.post("/listings",async(req,res)=>{
+app.post("/listings",wrapAsync(async(req,res,next)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"bad request");  //hopscotch-postreq-/listings ;similar thing can be dodne at update listing where req.body used
+    }
+    
     const {title,description,image,price,country,location}=req.body;
     const newListing=new Listing({
         title,description,image,price,country,location
@@ -107,7 +132,7 @@ app.post("/listings",async(req,res)=>{
 
    res.redirect("/listings");
 
-})
+}))
 // app.get("/listings",(req,res)=>{
 //     let sampleListing=new Listing(
 //         {
@@ -122,6 +147,14 @@ app.post("/listings",async(req,res)=>{
 //     console.log("data saved");
 //     res.send("data saved ");
 // })
+
+app.use((req,res,next)=>{
+    next(new ExpressError(409,"Page not found..."))
+})
+app.use((err,req,res,next)=>{
+    let{statusCode=500,message="something unknown error"}=err;
+    res.render("./listings/error.ejs",{statusCode,message});
+})
 app.listen(8080,(req,res)=>{
     console.log("server is running");
 })
---
+++ schema.js
@@ -0,0 +1,11 @@
+const Joi = require('joi');
+module.exports.listingSchema = Joi.object({
+    listing :Joi.object({
+        title:Joi.string().required(),
+        description:Joi.string().required(),
+        price:Joi.string().required().min(0),
+        image:Joi.number().allow("",null),
+        location:Joi.string().required(),
+        country:Joi.string().required(),
+    })
+})
